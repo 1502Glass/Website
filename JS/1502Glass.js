2e8e3b8fd107421849a1d91164a731714a6cbae7
1502Glass <30363271+1502Glass@users.noreply.github.com>
2017-07-22 15:06:58 -0700

    Add files via upload

---
+++ JS/1502Glass.js
@@ -0,0 +1,30 @@
+/*globals browser, console, JQuery, $ */
+/*jslint plusplus: true */
+
+/*
+function adjustWidth() {
+    'use strict';
+    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    
+    if (w <= 500) {
+        document.getElementById('loginDiv').classList.remove('topBannerPad');
+    } else {
+        document.getElementById('loginDiv').classList.add('topBannerPad');
+    }
+}
+
+function buildInventoryTable() {
+    'use strict';
+    
+    
+}
+
+(function () {
+    'use strict';
+    adjustWidth();
+    buildInventoryTable();
+}());
+
+window.onresize = adjustWidth;
+*/
+
